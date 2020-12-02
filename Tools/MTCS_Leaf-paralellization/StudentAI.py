b94c55f7b95836073e160cd21ed023e33d5d436b
Tuan Truong <tuannt2@uci.edu>
2020-12-01 19:32:58 -0800

    Added leaf paralellization, update main AI

---
+++ Tools/MTCS_Leaf-paralellization/StudentAI.py
@@ -0,0 +1,259 @@
+from random import randint
+from BoardClasses import Move
+from BoardClasses import Board
+import multiprocessing as mp
+import math
+import time
+import copy
+#The following part should be completed by students.
+#Students can modify anything except the class name and exisiting functions and varibles.
+
+C = math.sqrt(math.sqrt(2)) # Exploration Parameter
+NS = 1000
+minVisit = 20
+num_processes = 5
+
+class TreeNode:
+    def __init__(self, move, player, parent=None):
+        self.move = move
+        self.parent = parent
+        self.child_node = []
+        self.simulation = 0
+        self.win = 0
+        self.player = player
+
+    def uct(self):
+        if self.simulation == 0:
+            return 9999
+        if self.parent:
+            return self.winrate() + C * math.sqrt(math.log(self.parent.simulation)/self.simulation)
+
+    def winrate(self):
+        if self.simulation == 0:
+            return 0
+        return self.win/self.simulation
+
+    def __repr__(self):
+        return "TreeNode: " + str(self.move) + " player: " + str(self.player)
+
+
+class StudentAI():
+    def __init__(self,col,row,p):
+        self.col = col
+        self.row = row
+        self.p = p
+        self.board = [Board(col,row,p) for i in range(num_processes)]
+        for board in self.board:
+            board.initialize_game()
+        self.color = ''
+        self.opponent = {1:2,2:1}
+        self.color = 2
+        self.search_lim = 5
+        self.current_node = TreeNode(None, self.color)
+        self.sim_total = 0
+        self.sim_counter = 0
+
+    def get_move(self,move):
+        if len(move) != 0:
+            # print("|" + str(move) + "|")
+            self.all_make_move(move, self.opponent[self.color])
+            #print("Player", self.opponent[self.color], "make move", move)
+            if len(self.current_node.child_node) != 0:
+                for child in self.current_node.child_node:
+                    if str(child.move) == str(move):
+                        self.current_node = child
+        else:
+            self.color = 1
+            self.current_node.player = self.color
+        bt = time.time()
+        for i in range(NS):
+            self.mcts(self.current_node)
+            #self.board.show_board()
+            #print("mcts counter:", i)
+        move = self.current_node.child_node[0]
+        print("self.mtcs:", time.time() - bt)
+        print("avg sim time:", self.sim_total / self.sim_counter)
+        for child in self.current_node.child_node:
+            if move.winrate() < child.winrate():
+                move = child
+        self.all_make_move(move.move, self.color)
+        # print("Player", self.color, "make move", move.move, "with a winrate of", move.winrate(), "simulated", move.simulation)
+        self.current_node = move
+        temp_moves = self.board[0].get_all_possible_moves(self.opponent[self.color])
+        if len(self.node.child_node) == 0:
+            for t_move in temp_moves:
+                for eachmove in t_move:
+                    self.node.child_node.append(TreeNode(eachmove, self.opponent[self.node.player], self.node))
+        return move.move
+
+    def all_make_move(self, move, player):
+        for board in self.board:
+            board.make_move(move, player)
+
+    def all_undo(self):
+        for board in self.board:
+            board.undo()
+
+    def mcts(self, node):
+        if node.simulation >= minVisit:
+            #print("depth:", depth)
+            node.simulation += num_processes
+            if not len(node.child_node):
+                #bt = time.time()
+                moves = self.board[0].get_all_possible_moves(node.player)
+                #print("self.board.get_all_possible_moves:", time.time() - bt)
+                for move in moves:
+                    for eachmove in move:
+                        node.child_node.append(TreeNode(eachmove, self.opponent[node.player], node))
+            # proceed
+            #bt = time.time()
+            next = self.mcts_selection(node)
+            #print("self.mcts_selection:", time.time() - bt)
+            #bt = time.time()
+            self.all_make_move(next.move, node.player)
+            #print("self.board.make_move", time.time() - bt)
+            #bt = time.time()
+            result = [self.board[0].is_win(node.player) * num_processes]
+            #print("self.board.is_win", time.time() - bt)
+            if result:
+                if result == self.opponent[node.player]: node.win += num_processes
+                elif result == node.player:
+                    next.win += num_processes
+                    next.simulation += num_processes
+                self.all_undo()
+                return result
+                    #self.board.show_board()
+            result = self.mcts(next)
+            #bt = time.time()
+            self.all_undo()
+            #print("self.board.undo()", time.time() - bt)
+            # propagate up
+            for res in result:
+                if res == self.opponent[node.player]:
+                    node.win += 1
+            return result
+        else:
+            bt = time.time()
+            result = self.simulate_wrapper(node.player)
+            bt = time.time() - bt
+            self.sim_total += bt
+            self.sim_counter += 1
+            #print("self.simulate", bt)
+            node.simulation += 1
+            if result == self.opponent[node.player]:
+                node.win += 1
+            #print("simulating", result)
+            return result
+
+
+
+    def mcts_selection(self, node): # Select optimal UCB node
+        current = node.child_node[0]
+        for child in node.child_node:
+            #print(current.uct())
+            if current.winrate() < child.winrate():
+                current = child
+        #print("player", node.player, "pick", current.move)
+        return current
+
+    def simulate_wrapper(self, player):
+        processes = []
+        result = []
+        for i in range(num_processes):
+            p = mp.Process(target=self.simulate, args=(player, self.board[i], result))
+            processes.append(p)
+            p.start()
+        for p in processes:
+            p.join()
+        for p in processes:
+            if p.is_alive():
+                p.terminate()
+
+    def simulate(self, player, board, result):
+        win = 0
+        counter = 0
+        # fake_board = Board(self.col, self.row, self.p)
+        # self.copy_board(fake_board)
+        #print("DIEN")
+        #fake_board.show_board()
+        #totaltime = 0
+        while win == 0:
+            moves = board.get_all_possible_moves(player)
+            if len(moves) == 1:
+                index = 0
+            elif len(moves) == 0:
+                win = self.opponent[player]
+                break
+            else:
+                index = randint(0, len(moves) - 1)
+            if len(moves[index]) == 1:
+                inner_index = 0
+            else:
+                inner_index = randint(0, len(moves[index]) - 1)
+            move = moves[index][inner_index]
+            board.make_move(move, player)
+            counter += 1
+            #bt = time.time()
+            if board.tie_counter >= board.tie_max:
+                win = -1
+            #totaltime += time.time() - bt
+            #print("self.board.is_win():", time.time() - bt)
+            player = self.opponent[player]
+
+        # #print("total time is_win:", totaltime)
+        # #bt = time.time()
+        for i in range(counter):
+            board.undo()
+        # #rint("total time undo:", time.time() - bt)
+        #fake_board.show_board()
+        #print("winner", win)
+        result.append(win)
+
+    #
+    # def copy_board(self, board):
+    #     """
+    #     EZ game
+    #     :return: ez board
+    #     """
+    #     board.tie_counter = self.board.tie_counter
+    #     board.tie_max = self.board.tie_max
+    #     #bt = time.time()
+    #     board.board = copy.deepcopy(self.board.board)
+    #     board.saved_move = copy.deepcopy(self.board.saved_move)
+    #     #print("Deepcopying", time.time() - bt)
+    #     board.black_count = self.board.black_count
+    #     board.white_count = self.board.white_count
+
+
+
+
+
+
+
+
+    # def is_win(self, board):
+    #     """
+    #     this function tracks if any player has won
+    #     @param :
+    #     @param :
+    #     @return :
+    #     @raise :
+    #     """
+    #     if board.tie_counter >= board.tie_max:
+    #         return -1
+    #     W_has_move = True
+    #     B_has_move = True
+    #     if len(board.get_all_possible_moves(1)) == 0:
+    #         if turn != 1:
+    #             B_has_move = False
+    #     elif len(board.get_all_possible_moves(2)) == 0:
+    #         if turn != 2:
+    #             W_has_move = False
+    #
+    #     if W_has_move and not B_has_move:
+    #         return 2
+    #     elif not W_has_move and B_has_move:
+    #         return 1
+    #     else:
+    #         return 0
+
--- src/StudentAI.py
+++ src/StudentAI.py
@@ -4,13 +4,16 @@ from BoardClasses import Board
 import math
 import time
 import copy
+import threading
+import multiprocessing as mp
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 
 C = math.sqrt(math.sqrt(2)) # Exploration Parameter
 NS = 1000
 minVisit = 20
-num_processes = 6
+num_thread = 5
+queue = mp.Queue()
 
 class TreeNode:
     def __init__(self, move, player, parent=None):
@@ -36,58 +39,191 @@ class TreeNode:
         return "TreeNode: " + str(self.move) + " player: " + str(self.player)
 
 
+
 class StudentAI():
+
     def __init__(self,col,row,p):
         self.col = col
         self.row = row
         self.p = p
-        self.board = Board(col,row,p)
-        self.board.initialize_game()
+        self.boards = []
+        for i in range(num_thread):
+            self.boards.append(Board(col, row, p))
+            self.boards[i].initialize_game()
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
-        self.search_lim = 5
-        self.current_node = TreeNode(None, self.color)
+        self.mcts_trees = [TreeNode(None, self.color) for i in range(num_thread)]
         self.sim_total = 0
         self.sim_counter = 0
 
     def get_move(self,move):
+        """
+        Return best next move
+        :param move: Previous move made by opponent
+        :return: AI's move
+        """
         if len(move) != 0:
             # print("|" + str(move) + "|")
-            self.board.make_move(move, self.opponent[self.color])
+            self.confirm_move(move, self.opponent[self.color])
+            self.update_node(move)
             #print("Player", self.opponent[self.color], "make move", move)
-            if len(self.current_node.child_node) != 0:
-                for child in self.current_node.child_node:
-                    if str(child.move) == str(move):
-                        self.current_node = child
         else:
             self.color = 1
-            self.current_node.player = self.color
-        bt = time.time()
-        for i in range(NS):
-            self.mcts(self.current_node)
-            #self.board.show_board()
-            #print("mcts counter:", i)
-        move = self.current_node.child_node[0]
-        print("self.mtcs:", time.time() - bt)
-        print("avg sim time:", self.sim_total / self.sim_counter)
-        for child in self.current_node.child_node:
-            if move.uct() < child.uct():
-                move = child
-        self.board.make_move(move.move, self.color)
+            for i in range(num_thread):
+                self.mcts_trees[i].player = self.color
+        # bt = time.time()
+        self.run_mcts(NS)
+        # print("self.run_mcts:", time.time() - bt)
+        #print("avg sim time:", self.sim_total / self.sim_counter)
+        move = self.pick_move()
+        self.confirm_move(move, self.color)
+        self.update_node(move)
+
+        # Expand in case new nodes have no child
+        temp_moves = self.boards[0].get_all_possible_moves(self.opponent[self.color])
+        for node in self.mcts_trees:
+            if len(node.child_node) == 0:
+                for t_move in temp_moves:
+                    for eachmove in t_move:
+                        node.child_node.append(TreeNode(eachmove, self.opponent[node.player], node))
+
         # print("Player", self.color, "make move", move.move, "with a winrate of", move.winrate(), "simulated", move.simulation)
-        self.current_node = move
-        return move.move
+        return move
 
 
-    def mcts(self, node):
+    def update_node(self, move):
+        """
+        Find index of node to udpate
+        :param current_node: current node
+        :param move: move to update
+        :return: index of child
+        """
+        index = 0
+        if len(self.mcts_trees[0].child_node) != 0:
+            for i, child in enumerate(self.mcts_trees[0].child_node):
+                if str(child.move) == str(move):
+                    index = i
+                    break
+            for i in range(len(self.mcts_trees)):
+                self.mcts_trees[i] = self.mcts_trees[i].child_node[index]
+                self.mcts_trees[i].parent = None
+
+    def confirm_move(self, move, player):
+        """
+        Make move
+        :return: Nun
+        """
+        for board in self.boards:
+            board.make_move(move, player)
+
+    def pick_move(self):
+        """
+        Pick move base on win rate
+        :return: Move object
+        """
+        child_node_evaluate = []
+        num_child = len(self.mcts_trees[0].child_node)
+        # print(self.mcts_trees[0].move)
+        # print(list(i.move for i in self.mcts_trees[0].child_node))
+        """
+        Adding num_sim and num_win for each child node from all tree
+        Store in child_node_list list
+        """
+        for i in range(num_child):
+            num_sim = 0
+            num_win = 0
+            for j in range(num_thread):
+                num_sim += self.mcts_trees[j].child_node[i].simulation
+                num_win += self.mcts_trees[j].child_node[i].win
+            """
+            Change for different picking method
+            Currently: win rate
+            """
+
+            child_node_evaluate.append(num_win/(num_sim if num_sim else 1))
+        best = child_node_evaluate[0]
+        index = 0
+        for i, item in enumerate(child_node_evaluate):
+            if best < item:
+                best = item
+                index = i
+        return self.mcts_trees[0].child_node[index].move
+
+
+
+    def run_mcts(self, number_sim):
+        """
+        Running mcts with clone trees using multithreading
+        :param number_sim: number of simulation
+        :return: None
+        """
+        processes = []
+
+        for i, tree in enumerate(self.mcts_trees):
+            p = mp.Process(target=self.mcts_wrapper, args=(tree, number_sim, self.boards[i]))
+            processes.append(p)
+            p.start()
+        # print(processes)
+        i = 0
+        while True:
+            running = any(p.is_alive() for p in processes)
+            while not queue.empty():
+                self.mcts_trees[i] = queue.get()
+                i += 1
+            if not running:
+                break
+        for p in processes:
+            # print("initiated")
+            p.join()
+            # print(p, "JOINED")
+        # print("JOINED")
+        for p in processes:
+            if p.is_alive():
+                p.terminate()
+                # print("terminated bitch")
+            # else:
+            #     print("exit code", str(p.exitcode))
+        # for p in processes:
+        #     p.kill()
+        # print("finished processes")
+        # self.mcts_trees = [queue.get() for p in processes]
+        # print(self.mcts_trees[0].child_node[0])
+
+
+    def mcts_wrapper(self, node, number_sim, board):
+        """
+        Wrapper for self.mcts
+        :param node: current node
+        :param number_sim: number of simulation
+        :return: None
+        """
+        bt = time.time()
+        # print("going in")
+        for i in range(number_sim):
+            self.mcts(node, board)
+        # print("fuck ye")
+        # for child in node.child_node:
+        #     print(child.simulation, end=" | ")
+        # print()
+        # print("Time taken:", time.time() - bt)
+        queue.put(node)
+        # print("putin")
+
+
+
+    def mcts(self, node, board):
+        """
+        One run of Monte Carlo Tree Search using recursive
+        :param node: current node
+        :param board: current broad
+        :return: N/A (result for recursive call)
+        """
         if node.simulation >= minVisit:
             #print("depth:", depth)
             node.simulation += 1
             if not len(node.child_node):
-                #bt = time.time()
-                moves = self.board.get_all_possible_moves(node.player)
-                #print("self.board.get_all_possible_moves:", time.time() - bt)
+                moves = board.get_all_possible_moves(node.player)
                 for move in moves:
                     for eachmove in move:
                         node.child_node.append(TreeNode(eachmove, self.opponent[node.player], node))
@@ -96,22 +232,22 @@ class StudentAI():
             next = self.mcts_selection(node)
             #print("self.mcts_selection:", time.time() - bt)
             #bt = time.time()
-            self.board.make_move(next.move, node.player)
+            board.make_move(next.move, node.player)
             #print("self.board.make_move", time.time() - bt)
             #bt = time.time()
-            result = self.board.is_win(node.player)
+            result = board.is_win(node.player)
             #print("self.board.is_win", time.time() - bt)
             if result:
                 if result == self.opponent[node.player]: node.win += 1
                 elif result == node.player:
                     next.win += 1
                     next.simulation += 1
-                self.board.undo()
+                board.undo()
                 return result
                     #self.board.show_board()
-            result = self.mcts(next)
+            result = self.mcts(next, board)
             #bt = time.time()
-            self.board.undo()
+            board.undo()
             #print("self.board.undo()", time.time() - bt)
             # propagate up
             if result == self.opponent[node.player]:
@@ -119,7 +255,7 @@ class StudentAI():
             return result
         else:
             bt = time.time()
-            result = self.simulate(node.player)
+            result = self.simulate(node.player, board)
             bt = time.time() - bt
             self.sim_total += bt
             self.sim_counter += 1
@@ -132,7 +268,12 @@ class StudentAI():
 
 
 
-    def mcts_selection(self, node): # Select optimal UCB node
+    def mcts_selection(self, node):
+        """
+        Select optimal UCB node
+        :param node: current node
+        :return: child node to explode (based on uct value)
+        """
         current = node.child_node[0]
         for child in node.child_node:
             #print(current.uct())
@@ -143,16 +284,22 @@ class StudentAI():
 
 
 
-    def simulate(self, player):
+    def simulate(self, player, board):
+        """
+        Simulate checker game
+        :param player: current player turn
+        :param board: current board
+        :return: winner (-1 for tie)
+        """
         win = 0
         counter = 0
         # fake_board = Board(self.col, self.row, self.p)
         # self.copy_board(fake_board)
-        #print("DIEN")
-        #fake_board.show_board()
+        # print("DIT ME TUAN")
+        # board.show_board()
         #totaltime = 0
         while win == 0:
-            moves = self.board.get_all_possible_moves(player)
+            moves = board.get_all_possible_moves(player)
             if len(moves) == 1:
                 index = 0
             elif len(moves) == 0:
@@ -165,10 +312,10 @@ class StudentAI():
             else:
                 inner_index = randint(0, len(moves[index]) - 1)
             move = moves[index][inner_index]
-            self.board.make_move(move, player)
+            board.make_move(move, player)
             counter += 1
             #bt = time.time()
-            if self.board.tie_counter >= self.board.tie_max:
+            if board.tie_counter >= board.tie_max:
                 win = -1
             #totaltime += time.time() - bt
             #print("self.board.is_win():", time.time() - bt)
@@ -177,13 +324,18 @@ class StudentAI():
         # #print("total time is_win:", totaltime)
         # #bt = time.time()
         for i in range(counter):
-            self.board.undo()
-        # #rint("total time undo:", time.time() - bt)
-        #fake_board.show_board()
+            board.undo()
+        # print("total time undo:", time.time() - bt)
+        # print("UNDO")
+        # board.show_board()print
         #print("winner", win)
         return win
 
-    #
+
+
+
+
+
     # def copy_board(self, board):
     #     """
     #     EZ game
